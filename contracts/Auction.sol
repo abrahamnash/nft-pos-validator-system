06f30cb8cf7b4908b756e7aae6e017d5c202f01f
Dr. Abraham Nash <61595494+abrahamnash@users.noreply.github.com>
2025-08-19 17:12:14 -0400

    Create Auction.sol

---
+++ contracts/Auction.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Auction is Ownable {
+    IERC721 public nftContract;
+    mapping(uint256 => uint256) public nftBids;
+    mapping(uint256 => address) public nftHighestBidder;
+
+    uint256 public auctionDuration = 14 days;
+
+    constructor(IERC721 _nftContract) {
+        nftContract = _nftContract;
+    }
+
+    function startAuction(uint256 tokenId, uint256 startingPrice) external onlyOwner {
+        require(nftContract.ownerOf(tokenId) == msg.sender, "Only owner can start auction");
+        nftBids[tokenId] = startingPrice;
+    }
+
+    function bidOnNFT(uint256 tokenId) external payable {
+        require(msg.value > nftBids[tokenId], "Bid too low");
+        nftBids[tokenId] = msg.value;
+        nftHighestBidder[tokenId] = msg.sender;
+    }
+
+    function endAuction(uint256 tokenId) external {
+        address winner = nftHighestBidder[tokenId];
+        require(winner != address(0), "No bids placed");
+
+        nftContract.safeTransferFrom(msg.sender, winner, tokenId);
+        payable(msg.sender).transfer(nftBids[tokenId]);
+    }
+}
